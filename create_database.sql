fd2ee52ad0ede0e3085f54203a7c99753623bc2c
mpelu <35860173+mpelu@users.noreply.github.com>
2019-05-02 14:21:35 -0400

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -1,3 +1,11 @@
 --comment
-use dbName;
-CREATE TABLE persons ( name varchar(20), birthDate date );
+--should there be notes independent of appointment/doctor?
+CREATE DATABASE APPTMANAGER;
+USE APPTMANAGER;
+CREATE TABLE Persons ( ); --pk id, first, last, birthdate date, FK PRESCRIPTIONS
+CREATE TABLE Offices ( ); -- doc name, location name, specialty, ADDRESS INC ROOM, EMAIL, PHONE
+CREATE TABLE Prescriptions ( NAME VARCHAR (30) ); 
+CREATE TABLE Appointments ( ); -- phone, address, location/doctor, datetime, person, notes
+CREATE TABLE Actions ( ); -- text, who, fk contact info
+CREATE TABLE PersonScrips ( ); -- full name, scrip, dosage, purpose, as needed/daily/twice daily
+
