6ef305aeafb8c8aae2d98cc07e1c5521fde2752b
mpelu <35860173+mpelu@users.noreply.github.com>
2019-05-03 20:50:41 -0400

    Column names

--- create_database.sql
+++ create_database.sql
@@ -1,11 +1,78 @@
 --comment
---should there be notes independent of appointment/doctor?
+--How to work with PeopleID if doctor vs office
 CREATE DATABASE APPTMANAGER;
 USE APPTMANAGER;
-CREATE TABLE Persons ( ); 
-CREATE TABLE Offices ( ); 
-CREATE TABLE Prescriptions ( ); 
-CREATE TABLE Appointments ( ); 
-CREATE TABLE Actions ( ); 
-CREATE TABLE PersonScrips ( ); 
+
+CREATE TABLE Persons ( 
+  PeopleID
+  FirstName
+  LastName
+  Type
+  ); 
+  
+CREATE TABLE Patients (
+  PatientID
+  FirstName
+  LastName
+  DateOfBirth
+  );
+  
+CREATE TABLE Doctors ( 
+  DoctorID
+  DocName
+  OfficeName
+  Specialization
+  Comments
+  Address1
+  Address2
+  City
+  Province
+  PostalCode
+  Phone
+  Email
+  Fax
+  ); 
+  
+CREATE TABLE Medications ( 
+  MedID
+  MedName
+  ); 
+  
+CREATE TABLE Issues (
+  IssueID
+  PatientID
+  IssueTitle
+  IssueText  
+  );
+  
+CREATE TABLE Appointments ( 
+  ApptID
+  PatientID
+  DoctorID
+  DateAndTime
+  Comments
+  ); 
+  
+CREATE TABLE NextActions ( 
+  ActionID
+  ApptID
+  ActionText
+  ); 
+  
+CREATE TABLE PeopleActions (
+  PeopleID
+  ActionID
+  );
+  
+CREATE TABLE PatientsMeds ( 
+  PatientID
+  MedID
+  Dosage
+  Instructions
+  ); 
+  
+CREATE TABLE ApptIssues (
+  ApptID
+  IssueID
+  );
 
